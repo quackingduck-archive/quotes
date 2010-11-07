c27c1894ad4de9a9b02e6500ad70e9ae54db2fcd
Myles Byrne <myles@myles.id.au>
2010-11-07 10:25:40 -0600

    whitespace

--- app.rb
+++ app.rb
@@ -13,16 +13,16 @@ get '/' do
 end
 
 class Quote
-  
+
   def self.all
     Dir[Sinatra::Application.root+'/quotes/*'].
     collect { |path| load(path) }.
     compact. # ignore the ones that fail to load
     sort_by { |q| q.date }.reverse
   end
-  
+
   class ParseError < StandardError ; end
-  
+
   def self.load(path)
     id = File.basename(path)
     begin
@@ -32,31 +32,31 @@ class Quote
       $stderr.puts "can't parse quote at: #{path}"
     end
   end
-  
+
   def self.parse(str)
     match = str.match(/(.+?)---\n(.+)/m)
     raise ParseError unless match
     match.captures
   end
-  
+
   def initialize(*args)
     @id, @body, @props = args
   end
-  
+
   attr_reader :id, :updated
-  
+
   def to_s; RDiscount.new(@body).to_html end
-  
+
   def method_missing(*args)
     return super unless args.size == 1
     name = args.first.to_s[/(.+?)\??$/,1]
     @props[name]
   end
-  
+
 end
 
 helpers do
-  
+
   def quotes ; Quote.all end
-  
+
 end
